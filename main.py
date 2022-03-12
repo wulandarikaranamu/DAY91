988090a944dde656ec3e76d4d0e258e57c4e366a
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-03-12 21:33:36 +0700

    komentar

---
+++ main.py
@@ -0,0 +1,71 @@
+#operator penugasan
+# =,+=,-=,*=,/=,%=,**=,//=
+
+nilai = 4
+nilai += 2
+print(nilai)
+nilai -= 1
+print(nilai)
+nilai *= 5
+print(nilai)
+nilai /= 2
+print(nilai)
+nilai %= 3
+print(nilai)
+nilai **= 1
+print(nilai)
+nilai //= 4
+print(nilai)
+
+#operator perbandingan
+# ==, !=, >, <, >=, <=
+
+# == & = 
+
+#contoh program
+data1 = 3
+data2 = 2
+print(data1 == data2)
+data1 = 2
+data2 = 2
+print(data1 == data2)
+print("="*20)
+data1 = 2
+data2 = 3
+print(data1 != data2)
+data1 = 3
+data2 = 3
+print(data1 != data2)
+print("="*20)
+data1 = 3
+data2 = 2
+print(data1 > data2)
+print("="*20)
+data1 = 2
+data2 = 3
+print(data1 < data2)
+print("="*20)
+data1 = 3
+data2 = 2
+print(data1 >= data2)
+print("="*20)
+data1 = 2
+data2 = 3
+print(data1 <= data2)
+
+#operator logika
+
+#and
+print("hasil dari true dan true adalah :",True and True)
+print("hasil dari false dan true adalah :",False and True)
+print("hasil dari true dan false adalah :",True and False)
+print("hasil dari false dan false adalah :",False and False)
+print("="*30)
+#or
+print("hasil dari true dan true adalah :",True or True)
+print("hasil dari false dan true adalah :",False or True)
+print("hasil dari true dan false adalah :",True or False)
+print("hasil dari false dan false adalah :",False or False)
+#not 
+print("hasil dari false adalah :",not False)
+print("hasil dari True adalah :",not True)
